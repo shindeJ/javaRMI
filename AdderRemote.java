6371caca4ef40e446be3eb1e7b90f4f3f7f51c00
shindeJ <72152037+shindeJ@users.noreply.github.com>
2020-11-06 22:33:24 +0530

    Add files via upload

---
+++ AdderRemote.java
@@ -0,0 +1,6 @@
+import java.rmi.*;
+import java.rmi.server.*;
+public class AdderRemote extends UnicastRemoteObject implements Adder{
+	AdderRemote() throws RemoteException {super();}
+	public int add(int x,int y){return x+y;}
+}
